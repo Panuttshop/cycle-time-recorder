4ccaf3cd9e420a192fa7cd3b785065175415016c
Panuttshop <panu.ttshop@gmail.com>
2025-10-16 13:10:36 +0700

    __init__.py

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,1 @@
-"""
-Views Package
-"""
-from views import main_entry, view_edit, analytics, export, admin, settings
-
-__all__ = ['main_entry', 'view_edit', 'analytics', 'export', 'admin', 'settings']
+__version__ = "1.0.0"
